3eb40fbf384a8d20c2c8010c36b8fadec6d5b4f6
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-11-21 15:30:52 -0800

    working on server, connection and .env files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+let sequelize;
+if(process.env.JAWSDB_URL){
+  sequelize= new Sequelize(process.env.JAWSDB_URL)
+} else{
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+    );
+  }
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/techmatchup', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-module.exports = mongoose.connection;
+module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -3,19 +3,34 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
-
+require('dotenv').config();
+const session = require("express-session");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const sequelize = require("./config/connection.js")
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require("./controllers");
+
+const PORT = process.env.PORT || 3017;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+      maxAge: 1000 * 60 * 60 * 2
+  },
+  store: new SequelizeStore({
+      db:sequelize
+  })
+}))
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes)
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
@@ -30,5 +45,10 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+  console.log('App listening on PORT ' + PORT);
   });
 });
